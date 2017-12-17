432169a0f3d1acbcf91247a059da2ba906839994
hugo <happyhugo@outlook.com>
2017-12-17 20:39:35 +0800

    change follow page

--- app/src/main/java/com/wen/hugo/followPage/FollowPageActivity.java
+++ app/src/main/java/com/wen/hugo/followPage/FollowPageActivity.java
@@ -3,12 +3,18 @@ package com.wen.hugo.followPage;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.wen.hugo.R;
 import com.wen.hugo.data.DataRepository;
 import com.wen.hugo.util.ActivityUtils;
 import com.wen.hugo.util.schedulers.SchedulerProvider;
+
+import static com.wen.hugo.followPage.FollowPageFragment.TYPE_FOLLOWER;
+import static com.wen.hugo.followPage.FollowPageFragment.TYPE_FOLLOWING;
 
 public class FollowPageActivity extends AppCompatActivity {
 
@@ -18,6 +24,19 @@ public class FollowPageActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.followpage_activity);
+
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    final ActionBar ab = getSupportActionBar();
+    Intent intent = getIntent();
+    int type = intent.getIntExtra(FollowPageFragment.TYPE, TYPE_FOLLOWER);
+    if(type==TYPE_FOLLOWER) {
+        ab.setTitle("我的粉丝");
+    }else if(type==TYPE_FOLLOWING){
+        ab.setTitle("我的关注");
+    }
+    ab.setDisplayHomeAsUpEnabled(true);
 
     FollowPageFragment followPageFragment =
             (FollowPageFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
@@ -40,5 +59,15 @@ public class FollowPageActivity extends AppCompatActivity {
     Intent intent = new Intent(context, FollowPageActivity.class);
     intent.putExtra(FollowPageFragment.TYPE, type);
     context.startActivity(intent);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        this.finish();
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
 }
